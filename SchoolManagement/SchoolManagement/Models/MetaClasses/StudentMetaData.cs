55cb65d2fa0dc0546e0b7644e66d1990e3c1cd4c
Matías López <matilpcsd227@gmail.com>
2021-11-15 10:10:27 -0300

    Se agregaron validaciones de los datos, se hicieron correcciones en el modelo de datos y se agrego una tabla para visualizar los estudiantes en cada curso.

---
+++ SchoolManagement/SchoolManagement/Models/MetaClasses/StudentMetaData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.Models
+{
+    public class StudentMetaData
+    {
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Date of Enrollment")]
+        public Nullable<System.DateTime> EnrollmentDate { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
+    }
+
+    [MetadataType(typeof(StudentMetaData))]
+    public partial class Student
+    {
+
+    }
+}
